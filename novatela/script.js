afda4219face5bf512fda7dc018c411e51346e78
IsaiasZaza <131933508+IsaiasZaza@users.noreply.github.com>
2023-06-18 23:59:54 -0300

    estudo DESAFIO

--- novatela/script.js
+++ novatela/script.js
@@ -1,4 +1,32 @@
-function toggleForm() {
-    var formulario = document.getElementById("meuFormulario");
-    formulario.classList.toggle("hidden");
-  }
+const btnmodallan = document.querySelector('#modal-lan');
+const btnocultarlan = document.querySelector('#ocultar-modal');
+const contModal = document.querySelector('#conteiner-modal');
+
+const nameinput = document.querySelector('#nome');
+const depoisinput = document.querySelector('#depois');
+const emailinput = document.querySelector('#email');
+const dateinput = document.querySelector('#date');
+
+btnmodallan.addEventListener('click', (e) => {
+    e.preventDefault();
+    contModal.classList.add('mostrar');
+})
+
+btnocultarlan.addEventListener('click', (e) =>{
+    e.preventDefault();
+    contModal.classList.remove('mostrar');
+    const empleado = document.querySelector('#mdchefe');
+    
+    const pname = document.createElement('p');
+    pname.textContent = nameinput.value;
+
+    const pdepois = document.createElement('p');
+    pdepois.textContent = depoisinput.value;
+
+    const pemail = document.createElement('p');
+    pemail.textContent = emailinput.value;
+
+    const pdate = document.createElement('p');
+    pdate.textContent = dateinput.value;
+
+})
